9600a6ef02bb327091523313e8ac499fe4c25960
Alex Hoppen <ahoppen@apple.com>
2021-02-12 18:08:47 +0100

    Move isTokenKeyword around

--- include/swift/Parse/Token.h
+++ include/swift/Parse/Token.h
@@ -197,13 +197,7 @@ public:
   }
 
   /// True if the token is any keyword.
-  bool isKeyword() const {
-    switch (Kind) {
-#define KEYWORD(X) case tok::kw_##X: return true;
-#include "swift/Syntax/TokenKinds.def"
-    default: return false;
-    }
-  }
+  bool isKeyword() const { return isTokenKeyword(Kind); }
 
   /// True if the token is any literal.
   bool isLiteral() const {
